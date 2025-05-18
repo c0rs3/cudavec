1b1e4c57ce78bd4da7b426501a972d22b342ede8
Tuna Arıyürek <ariyurektuna@gmail.com>
2025-05-18 11:37:40 +0300

    kernel warmup and tweaks

--- kernel.cu
+++ kernel.cu
@@ -1,5 +1,12 @@
-﻿#include "utils.h"
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <functional>
+#include <vector>
+#include "benchmark.h"
 #include <iostream>
+
+__global__ static void KernelWarmup() {
+}
 
 __global__ static void addKernel(int* c, const int* a, const int* b, int size) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -43,32 +50,294 @@ __global__ static void divEqualsKernel(int* c, const int* a, const int& b, int s
 	}
 }
 
+template <typename Ty_, typename KernelFunc>
+__host__ std::vector<Ty_> performOperastor(const std::vector<Ty_>& a, const std::vector<Ty_>& b, KernelFunc kernelFunction) {
+	// cudastatus for tracking errors
+	cudaError_t cudaStatus = cudaSuccess;
+
+	// Set device (GPU)
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to set device! (incompatible GPU?)" << std::endl;
+		return {};
+	}
+
+	// Device pointers
+	Ty_* dev_a = nullptr, * dev_b = nullptr;
+
+	// Vector size
+	size_t size = a.size() > b.size() ? b.size() : a.size();
+
+	// Pinned memory pointer
+	Ty_* c, * foo = new int();
+
+	// CUDA stream
+	cudaStream_t stream;
+	cudaStatus = cudaStreamCreate(&stream);
+
+
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to create stream!" << std::endl;
+		cudaStreamDestroy(stream);
+		return {};
+	}
+
+	// Kernel launch configuration
+	int threadsPerBlock = 1024;
+	int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+	kernelFunction << <blocksPerGrid, threadsPerBlock, 0, stream >> > (c, dev_a, dev_b, size);
+
+	// Synchronize the stream to ensure all tasks are complete
+	cudaStatus = cudaStreamSynchronize(stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to synchronize streams!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+
+	// Cleanup
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFreeHost(c);
+	delete foo;
+	cudaStatus = cudaStreamDestroy(stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to destroy stream!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+}
+
+template <typename Ty_, typename KernelFunc>
+__host__ std::vector<Ty_> performOperator(const std::vector<Ty_>& a, const std::vector<Ty_>& b, KernelFunc kernelFunction) {
+
+	// cudastatus for tracking errors
+	cudaError_t cudaStatus = cudaSuccess;
+
+	// Set device (GPU)
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to set device! (incompatible GPU?)" << std::endl;
+		return {};
+	}
+
+	// Device pointers
+	Ty_* dev_a = nullptr, * dev_b = nullptr;
+
+	// Vector size
+	size_t size = a.size() > b.size() ? b.size() : a.size();
+
+	// Pinned memory pointer
+	Ty_* c, * foo = new int();
+
+	// CUDA stream
+	cudaStream_t stream;
+	cudaStatus = cudaStreamCreate(&stream);
+
+
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to create stream!" << std::endl;
+		cudaStreamDestroy(stream);
+		return {};
+	}
+
+	// Allocate pinned host memory
+	cudaMallocHost(&c, size * sizeof(Ty_));
+
+	// Allocate device memory
+	cudaMalloc(&dev_a, size * sizeof(Ty_));
+	cudaMalloc(&dev_b, size * sizeof(Ty_));
+
+	// Copy data from host to device asynchronously
+	cudaStatus = cudaMemcpyAsync(dev_a, a.data(), size * sizeof(Ty_), cudaMemcpyHostToDevice, stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed memcpy!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+	cudaStatus = cudaMemcpyAsync(dev_b, b.data(), size * sizeof(Ty_), cudaMemcpyHostToDevice, stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed memcpy!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+
+	// Kernel launch configuration
+	dim3 blocksPerGrid(1024);
+	dim3 threadsPerBlock(size / 1024);
+	kernelFunction << <blocksPerGrid, threadsPerBlock, 0, stream >> > (c, dev_a, dev_b, size);
+
+	// Synchronize the stream to ensure all tasks are complete
+	cudaStatus = cudaStreamSynchronize(stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to synchronize streams!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+
+	std::vector<Ty_> res(c, c + size);
+
+	// Cleanup
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFreeHost(c);
+	delete foo;
+	cudaStatus = cudaStreamDestroy(stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to destroy stream!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+
+	return res;
+}
+
+template <typename Ty_, typename KernelFunc>
+std::vector<Ty_> performOperator(const std::vector<Ty_>& a, const Ty_& b, KernelFunc kernelFunction) {
+	// cudastatus for tracking errors
+	cudaError_t cudaStatus = cudaSuccess;
+
+	// Set device (GPU)
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to set device! (incompatible GPU?)" << std::endl;
+		return {};
+	}
+
+	// Device pointers
+	Ty_* dev_a = nullptr, * dev_b = nullptr;
+
+	// Vector size
+	size_t size = a.size();
+
+	// Pinned memory pointer
+	Ty_* c;
+
+	// CUDA stream
+	cudaStream_t stream;
+	cudaStatus = cudaStreamCreate(&stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to create stream!" << std::endl;
+		cudaStreamDestroy(stream);
+		return {};
+	}
+
+	// Allocate pinned host memory
+	cudaMallocHost(&c, size * sizeof(Ty_));
+
+	// Allocate device memory
+	cudaMalloc(&dev_a, size * sizeof(Ty_));
+	cudaMalloc(&dev_b, sizeof(Ty_));
+
+	// Copy data from host to device asynchronously
+	cudaStatus = cudaMemcpyAsync(dev_a, a.data(), size * sizeof(Ty_), cudaMemcpyHostToDevice, stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed memcpy!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+	cudaStatus = cudaMemcpyAsync(dev_b, &b, sizeof(Ty_), cudaMemcpyHostToDevice, stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed memcpy!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+
+	// Kernel launch configuration
+	int threadsPerBlock = 1024;
+	int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+	kernelFunction << <blocksPerGrid, threadsPerBlock, 0, stream >> > (c, dev_a, dev_b, size);
+
+	// Synchronize the stream to ensure all tasks are complete
+	cudaStatus = cudaStreamSynchronize(stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to synchronize streams!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+
+	std::vector<Ty_> res(c, c + size);
+
+	// Cleanup
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFreeHost(c);
+	cudaStatus = cudaStreamDestroy(stream);
+
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to destroy stream!" << std::endl;
+		return {};
+	}
+
+	return res;
+}
 
 template<typename Ty_>
 std::vector<Ty_> operator+(const std::vector<Ty_>& left, const std::vector<Ty_>& right) {
-	std::vector<Ty_> res;
-	res.resize(left.size());
+	std::vector<Ty_> res(left.size());
 	for (size_t i = 0; i < left.size(); i++) {
 		res[i] = left[i] + right[i];
 	}
 	return res;
+	// return performOperator(left, right, addKernel);
+}
+
+template<typename Ty_>
+std::vector<Ty_> operator+(const std::vector<Ty_>& left, const Ty_& right) {
+	return performOperator(left, right, addKernel);
 }
 
 template<typename Ty_>
 std::vector<Ty_> operator*(const std::vector<Ty_>& left, const std::vector<Ty_>& right) {
-	std::vector<Ty_> res;
-	res.resize(left.size());
-	for (size_t i = 0; i < left.size(); i++) {
-		res[i] = left[i] * right[i];
-	}
-	return res;
+	return performOperator(left, right, addKernel);
 }
 
 
 int main() {
+	{
+	KernelWarmup << <1, 1 >> > ();
+	}
 	const size_t size = 1 << 20;
 	std::vector<int> A(size);
 	std::vector<int> B(size);
+	std::vector<int> C = {1, 2, 3};
+	std::vector<int> D = {1, 2, 3};
 
 	for (size_t i = 0; i < size; ++i) {
 		A[i] = i;
@@ -76,14 +345,21 @@ int main() {
 	}
 	{
 		benchmark::Timer<float> Timer;
-		std::vector<int> res = A + B;
-
+		A + B;
 	}
 	{
 		benchmark::Timer<float> Timer;
-		std::vector<int> res = performOperator(A, B, addKernel);
-
+		performOperator(A, B, addKernel);
 	}
+	{
+		benchmark::Timer<float> Timer;
+		performOperator(A, B, addKernel);
+	}
+	{
+		benchmark::Timer<float> Timer;
+		performOperator(A, B, mulKernel);
+	}
+	
 
 	std::cout << std::endl;
 
