8566b889d84057ab4ba434897d4f1e38885948e4
Tuna Arıyürek <ariyurektuna@gmail.com>
2025-05-22 22:38:05 +0300

    todo matrix2x2 mult

--- kernel.cu
+++ kernel.cu
@@ -5,46 +5,58 @@
 #include "benchmark.h"
 #include <iostream>
 
+typedef std::vector<std::vector<int>> Matrix2x2;
+
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::flush;
+
 __global__ static void KernelWarmup() {
 }
 
 template<typename Ty_>
-__global__ static void addKernel(Ty_* c, const Ty_* a, const Ty_* b, int size) {
+__global__ static void addKernel(Ty_* c, const Ty_* a, const Ty_* b, unsigned int size) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i < size) {
 		c[i] = a[i] + b[i];
 	}
 }
 
-__global__ static void mulKernel(int* c, const int* a, const int* b, int size) {
+template<typename Ty_>
+__global__ static void mulKernel(Ty_* c, const Ty_* a, const Ty_* b, unsigned int size) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i < size) {
 		c[i] = a[i] * b[i];
 	}
 }
 
-__global__ static void divKernel(int* c, const int* a, const int* b, int size) {
+template<typename Ty_>
+__global__ static void divKernel(Ty_* c, const Ty_* a, const Ty_* b, unsigned int size) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i < size) {
 		c[i] = a[i] / b[i];
 	}
 }
 
-__global__ static void addEqualsKernel(int* c, const int* a, const int& b, int size) {
+template<typename Ty_>
+__global__ static void addEqualsKernel(Ty_* c, const Ty_* a, const Ty_& b, unsigned int size) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i < size) {
 		c[i] = a[i] + b;
 	}
 }
 
-__global__ static void mulEqualsKernel(int* c, const int* a, const int& b, int size) {
+template<typename Ty_>
+__global__ static void mulEqualsKernel(Ty_* c, const Ty_* a, const Ty_& b, unsigned int size) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i < size) {
 		c[i] = a[i] * b;
 	}
 }
 
-__global__ static void divEqualsKernel(int* c, const int* a, const int& b, int size) {
+template<typename Ty_>
+__global__ static void divEqualsKernel(Ty_* c, const Ty_* a, const Ty_& b, unsigned int size) {
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	if (i < size) {
 		c[i] = a[i] / b;
@@ -52,23 +64,26 @@ __global__ static void divEqualsKernel(int* c, const int* a, const int& b, int s
 }
 
 // TODO add more operators
+__global__ void matrix_multiplication_kernel(int* A, int* B, int* C, unsigned int M, unsigned int N, unsigned int K) {
+	for (size_t i = 0; i < M; i++) {
+		for (size_t k = 0; k < N; k++) {
 
-__global__ void matrix_multiplication_kernel(const float* A, const float* B, float* C, int M, int N, int K) {
-
+		}
+	}
 }
 
-__host__ void CUDAContextInit() {
+__host__ void CUDAContextInit(int device = 0) {
 	// cudastatus for tracking errors
 	cudaError_t cudaStatus = cudaSuccess;
 
 	// Set device (GPU)
-	cudaStatus = cudaSetDevice(0);
+	cudaStatus = cudaSetDevice(device);
 	if (cudaStatus != cudaSuccess) {
 		std::cerr << "Failed to set device! (incompatible GPU?)" << std::endl;
 		return;
 	}
 
-	KernelWarmup << <1, 1>> > ();
+	KernelWarmup << <1, 1 >> > ();
 	cudaDeviceSynchronize();
 }
 
@@ -172,7 +187,99 @@ __host__ std::vector<Ty_> performOperator(const std::vector<Ty_>& a, const std::
 }
 
 template <typename Ty_, typename KernelFunc>
-std::vector<Ty_> performOperator(const std::vector<Ty_>& a, const Ty_& b, KernelFunc kernelFunction) {
+__host__ std::vector<Ty_> performOperator(const std::vector<Ty_>& a, const Ty_& b, KernelFunc kernelFunction) {
+	// cudastatus for tracking errors
+	cudaError_t cudaStatus = cudaSuccess;
+
+	// Set device (GPU)
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to set device! (incompatible GPU?)" << std::endl;
+		return {};
+	}
+
+	// Device pointers
+	Ty_* dev_a = nullptr, * dev_b = nullptr;
+
+	// Vector size
+	size_t size = a.size();
+
+	// Pinned memory pointer
+	Ty_* c;
+
+	// CUDA stream
+	cudaStream_t stream;
+	cudaStatus = cudaStreamCreate(&stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to create stream!" << std::endl;
+		cudaStreamDestroy(stream);
+		return {};
+	}
+
+	// Allocate pinned host memory
+	cudaMallocHost(&c, size * sizeof(Ty_));
+
+	// Allocate device memory
+	cudaMalloc(&dev_a, size * sizeof(Ty_));
+	cudaMalloc(&dev_b, sizeof(Ty_));
+
+	// Copy data from host to device asynchronously
+	cudaStatus = cudaMemcpyAsync(dev_a, a.data(), size * sizeof(Ty_), cudaMemcpyHostToDevice, stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed memcpy!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+	cudaStatus = cudaMemcpyAsync(dev_b, &b, sizeof(Ty_), cudaMemcpyHostToDevice, stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed memcpy!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+
+	// Kernel launch configuration
+	int threadsPerBlock = 1024;
+	int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
+	kernelFunction << <blocksPerGrid, threadsPerBlock, 0, stream >> > (c, dev_a, dev_b, size);
+
+	// Synchronize the stream to ensure all tasks are complete
+	cudaStatus = cudaStreamSynchronize(stream);
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to synchronize streams!" << std::endl;
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFreeHost(c);
+		cudaStreamDestroy(stream);
+
+		return {};
+	}
+
+	std::vector<Ty_> res(c, c + size);
+
+	// Cleanup
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFreeHost(c);
+	cudaStatus = cudaStreamDestroy(stream);
+
+	if (cudaStatus != cudaSuccess) {
+		std::cerr << "Failed to destroy stream!" << std::endl;
+		return {};
+	}
+
+	return res;
+}
+
+template <typename Ty_>
+__host__ std::vector<Ty_> matrixMul(const std::vector<Ty_>&a, const Ty_ & b) {
 	// cudastatus for tracking errors
 	cudaError_t cudaStatus = cudaSuccess;
 
@@ -279,8 +386,47 @@ std::vector<Ty_> operator*(const std::vector<Ty_>& left, const std::vector<Ty_>&
 }
 
 
+
+// to multiply two matrices
+Matrix2x2 mult(const Matrix2x2& arr, const Matrix2x2& brr) {
+	int n = arr.size();
+
+	// to store the resultant matrix
+	Matrix2x2 res(n, std::vector<int>(n, 0));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				res[i][j] += arr[i][k] * brr[k][j];
+			}
+		}
+	}
+
+	return res;
+}
+
 int main() {
 	CUDAContextInit();
+	const size_t size = 2;
+	std::vector<std::vector<int>> A(size);
+	A[0] = { 1, 2 };
+	A[1] = { 3, 4 };
+	std::vector<std::vector<int>> B(size);
+	B[0] = { 5, 6 };
+	B[1] = { 7, 8 };
+	std::vector<std::vector<int>> C(size);
+	C[0] = { 0, 0 };
+	C[1] = { 0, 0 };
+
+	std::vector<std::vector<int>> res = mult(A, B);
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++) {
+			cout << res[i][j] << " ";
+		}
+		cout << endl;
+	}
+	// matrix_multiplication_kernel(A.data()->data(), B.data()->data(), C.data()->data(), A.size(), A[0].size(), B.size());
+#ifdef PERF_DEBUG
 	const size_t size = 1 << 20;
 	std::vector<int> A(size);
 	std::vector<int> B(size);
@@ -294,6 +440,6 @@ int main() {
 		benchmark::Timer<float> timer;
 		performOperator(A, B, addKernel<int>);
 	}
-
+#endif
 	return 0;
 }
